daececc17bc4b503b9b53ded600ddac8e8518fa5
haziqusman98 <hrana.bscs17seecs@seecs.edu.pk>
2021-03-09 04:36:40 +0500

    updated

--- textToAsl/views.py
+++ textToAsl/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Sign
 
-# Create your views here.
+@api_view(['GET'])
+def get_signs(request):
+    text = request.data.get('text',None)
+    signs=[]
+    if text is not None:
+        for char in text:
+            signs.append(Sign.objects.get(character=char))
+        return Response(signs)
+
+
