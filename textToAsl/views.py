aa28204554897f12dcbe185884493fc537895661
haziqusman98 <hrana.bscs17seecs@seecs.edu.pk>
2021-03-09 17:58:13 +0500

    updated views

--- textToAsl/views.py
+++ textToAsl/views.py
@@ -7,9 +7,9 @@ from .serializers import SignSerializer
 
 @api_view(['GET'])
 def get_signs(request):
-    text = request.data.get('text',None)
+    text = request.GET['text']
     signs=[]
-    if text is not None:
+    if text:
         for char in text:
             signs.append(SignSerializer(Sign.objects.filter(character__iexact=char).first()).data)
         return Response(signs)
