4f5db073dacbc9da6a07a8817724e0f128dfc4e9
haziqusman98 <hrana.bscs17seecs@seecs.edu.pk>
2021-03-09 15:57:05 +0500

    Update models.py

--- textToAsl/models.py
+++ textToAsl/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.core.exceptions import FieldDoesNotExist
 
 class Sign(models.Model):
-    character = models.CharField(max_length=1)
-    image = models.URLField(max_length=200)
+    character=models.CharField(max_length=1)
+    image=models.URLField(max_length=200)
     
     def __str__(self):
         return f'{self.character}'
