35cfbece11bc151c61967605ebaef3d1b3efc515
haziqusman98 <hrana.bscs17seecs@seecs.edu.pk>
2021-05-10 17:58:18 +0500

    Update views.py

--- textToAsl/views.py
+++ textToAsl/views.py
@@ -25,8 +25,8 @@ def get_gloss(request):
 
 @api_view(['POST'])
 def verify_blob(request):
-    vid = request.data.get("vid")
+    vid = request.FILES.get("vid")
     # f = open("request.txt",'w')
     # f.write(request)
     vidcap = cv2.VideoCapture(vid)
-    return Response({vidcap.read()[0]:request.FILES.get('vid')})
+    return Response({vidcap.read()[0]:vid})
